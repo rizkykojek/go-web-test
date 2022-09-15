5c8404604efbe9a544a7161904f5f3913e6f3c77
Rizky Maulana <rizki.maulana@happyfresh.com>
2022-09-15 10:50:44 +0700

    Add handler response

---
+++ handler_test.go
@@ -0,0 +1,23 @@
+package go_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(writer, "Hello")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
